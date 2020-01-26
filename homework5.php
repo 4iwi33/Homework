43f572059ec38c398d3387209b31f5f27af77b58
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-01-26 20:19:42 +0300

    creat homework5
    add exc

---
+++ homework5.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>homework5</title>
+</head>
+
+<body>
+    <?php
+    /*Написать калькулятор, сохраняющий историю вычислений в текстовом файле.
+    Пример содержимого файла:
+    2*3=6
+    5*5=25
+    6*5=30*/
+
+    
+
+
+
+
+    ?>
+</body>
+
+</html>
