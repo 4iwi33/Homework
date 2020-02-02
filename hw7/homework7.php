482faad56fe083bf91ce17995297462c9a5905a5
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-02-02 19:56:28 +0300

    create & rename

--- hw7/homework7.php
+++ hw7/homework7.php
@@ -6,12 +6,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>homework7</title>
+    <style>
+        .list {
+            background-color: #E6E4E4;
+            width: 250px;
+            height: 170px;
+            padding: 15px;
+            margin: auto;
+        }
+
+        .listdown {
+            background-color: #fff;
+            height: 200px
+            font-size: 15px;
+            padding: 15px;
+        }
+
+        .list p {
+            text-align: justify;
+            font-size: 22px;
+        }
+
+        div {
+            border-radius: 1em;
+            box-shadow: 0 0 5px;
+            padding: 5px;
+        }
+
+        input {
+            border-radius: 1em;
+            background-color: #2C96E3;
+        }
+    </style>
 </head>
 
 <body>
-    <?
+    <form action="" method="get" target="_blank">
+        <div class="list">
+            <p>What is your qeustion ?</p>
+            <div class="listdown">
+                <input type="hidden" name=vote value=1>69% Don't have one<br>
+                <input type="hidden" name=vote value=2>22% Why?<br>
+                <input type="hidden" name=vote value=3>0% When?<br>
+                <input type="hidden" name=vote value=4>9% Where?<br>
 
-    ?>
+            </div>
+        </div>
+    </form>
+
 </body>
 
 </html>
