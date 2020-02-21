0b3b0b3f116e7f8c6e5f8623ac960321165a088f
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-02-21 19:07:04 +0300

    change script

--- hw11/hww11.php
+++ hw11/hww11.php
@@ -1,22 +1,19 @@
-<pre>
 <?php
-
-// print_r($_FILES)
-
-?>
-</pre>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php</title>
-</head>
-
-<body>
-    <p>File was send!</p>
-
-</body>
-
-</html>
+if (is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+    copy($_FILES['userfile']['tmp_name'], "file\\" . $_FILES['userfile']['name']);
+}
+$arr = scandir("file");
+for ($i = 2; $i < count($arr); $i++) {
+    switch (explode(".", $arr[$i])[1]) {
+        case 'docx':
+            $icon = 'docx.png';
+            break;
+        case 'pdf':
+            $icon = 'pdf.png';
+            break;
+        default:
+            $icon = 'sm.png';
+            break;
+    }
+    echo "<a href='file\\$arr[$i]'><img src='$icon' width='16' height='16'>$arr[$i]</a><br>";
+}
