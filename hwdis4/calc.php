c0bf9a71030024893628b89aa267c5db7df5e0ba
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-04-17 18:05:04 +0300

    Update calc.php

--- hwdis4/calc.php
+++ hwdis4/calc.php
@@ -1,17 +1,21 @@
 <?php
 include("autoload.php");
+
 $result = new Square(6);
 echo "квадрат <br>";
 echo "периметр квадрата " . $result->perimetr() . "<br>";
 echo "площадь квадрата " . $result->area() . "<br>";
+
 $rectangle = new Rectangle(5, 6);
 echo "прямоугольник <br>";
 echo "периметр прямоугольника " . $rectangle->perimetr() . "<br>";
 echo "площадь прямоугольника " . $rectangle->area() . "<br>";
+
 $triangle = new Triangle(3, 4, 5);
 echo "треугольник <br>";
 echo "периметр треугольника " . $triangle->perimetr() . "<br>";
 echo "площадь треугольника " . $triangle->area() . "<br>";
+
 $circle = new Circle(5);
 echo "окружность <br>";
 echo "периметр окружности " . $circle->perimetr() . "<br>";
